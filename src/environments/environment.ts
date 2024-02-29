98cf6775f110f54031def1bddb56271597cb6c59
l3lacksmithl3 <122351437+l3lacksmithl3@users.noreply.github.com>
2024-02-29 20:42:55 +0700

    new

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: false,
 
   API:"http://localhost:8888"
+  // API:"https://0d05-2001-44c8-45cc-f00b-64c5-8ba3-cc29-cfed.ngrok-free.app"
+  // https://ad05-2001-44c8-4321-131c-c19d-fa03-bb07-3ad5.ngrok-free.app/
 };
