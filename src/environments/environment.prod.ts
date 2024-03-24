83e8c085c4b47311492fbf652700102dbfdae951
good5day <kittinan.boat@gmail.com>
2024-03-24 16:15:32 +0700

    k

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   API:"https://api_web.sodacanhomelab.uk",
   FILE_UPLOAD:"https://api_file.apmedexplus.com/upload/new",
-  FILE_DELETE:"https://api_file.apmedexplus.com/delete"
+  FILE_DELETE:"https://api_file.apmedexplus.com/delete",
 };
